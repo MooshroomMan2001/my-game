501086f36cf4077931cbfa81a7babc2b18b205d5
MooshroomMan2001 <ewb@unassigned-hostname>
2014-04-08 17:35:19 -0400

    Added smarter collision detection

--- project.js
+++ project.js
@@ -10,99 +10,119 @@ var barfX = 1;
 var barfFired = false;
 var barfY = 150;
 var x = 625;
+var y = 225; 
 
 var enemyOne = {
-	x: 625
-    };
+    x: 625,
+    y: 225
+};
+
 var enemyTwo = {
-	x: 700
-    };
+    x: 700,
+    y: 225
+};
+
 var enemyThree = {
-	x: 800
-    };
+    x: 800,
+    y: 225
+};
+
+var keyPressed = function() {
+}
 
 
-    
+// this code is executed once when the program is started
+var setup = function() {
+    audio = new Audio("Barf.mp3");
+    music = new Audio("mixdown.wav");
+    music.controls = true;
+    music.loop = true;
+    music.autoplay = true;
+    document.body.appendChild(music);
+    img = loadImage("Background.png");
+    dog = loadImage("Untitled.png");
+    // set up the size of the canvas (you probably don't want to change this!)
+    size(625, 450);
+};
 
-
-    // this code is executed once when the program is started
-    var setup = function() {
-	audio = new Audio("Barf.mp3");
-	music = new Audio("mixdown.wav");
-	music.controls = true;
-	music.loop = true;
-	music.autoplay = true;
-	document.body.appendChild(music);
-	img = loadImage("Background.png");
-	dog = loadImage("Untitled.png");
-	// set up the size of the canvas (you probably don't want to change this!)
-	size(625, 450);
-
-	
-    };
 //mouse clicked function
-    var mouseClicked = function(){
-	barfFired = true;
-	barfX = 1;
-	audio.play();
+var mouseClicked = function(){
+    barfFired = true;
+    barfX = 1;
+    audio.play();
+};
 
+var moveEnemy = function(enemy) {
+    enemy.x = enemy.x - 1;
+}
 
-    };
+var drawEnemy = function(enemy) {
+    fill(255, 0, 0);
+    rect(enemy.x, enemy.y, 30, 30);
+}
 
-    var moveEnemy = function(enemy) {
-	enemy.x = enemy.x - 1;
+// override draw function, by default it will be called 60 times per second
+var draw = function() {  
+    background(200, 255, 100);
+    moveEnemy(enemyOne);
+    moveEnemy(enemyTwo);
+    moveEnemy(enemyThree);
+    image(img, 0, 0);
+    image(dog, mouseX, mouseY);
+    if(barfFired){
+	fill(166, 255, 0);
+	rect(barfX, barfY, 20, 20);
+	barfX = barfX + 5;
+	if(keyIsPressed && keyCode === UP){
+	    barfY -= 5;
+	} else if(keyIsPressed && keyCode === DOWN){
+	    barfY += 5;
+	}
     }
 
-    var drawEnemy = function(enemy) {
-	fill(255, 0, 0);
-	rect(enemy.x, 200, 30, 30);
+    drawEnemy(enemyOne);
+    drawEnemy(enemyTwo);
+    drawEnemy(enemyThree);
+
+    if(enemyOne.x < 1){
+	enemyOne.x = 625;
+
     }
 
-    // override draw function, by default it will be called 60 times per second
-    var draw = function() {  
-	background(200, 255, 100);
-	moveEnemy(enemyOne);
-	moveEnemy(enemyTwo);
-	moveEnemy(enemyThree);
-	image(img, 0, 0);
-	image(dog, mouseX, mouseY);
-	if(barfFired){
-	    fill(166, 255, 0);
-	    rect(barfX, barfY, 20, 20);
-	    barfX = barfX + 5;
-	    if(keyPressed && keyCode === UP){
-		barfY -= 5;
-	    } else if(keyPressed && keyCode === DOWN){
-		barfY += 5;
-	    }
-	}
-	drawEnemy(enemyOne);
-	drawEnemy(enemyTwo);
-	drawEnemy(enemyThree);
-	if(enemyOne.x < 1){
-	    enemyOne.x = 625;
+    if(enemyTwo.x < 1){
+	enemyTwo.x = 700;
+    }
 
-	}
-	if(enemyTwo.x < 1){
-	    enemyTwo.x = 700;
-	}
-	if(enemyThree.x < 1){
-	    enemyThree.x = 800;
+    if(enemyThree.x < 1){
+	enemyThree.x = 800;
 
-	}
-	if(barfX > enemyOne.x){
-	    enemyOne.x = 625;
-	}
-	if(barfX > enemyTwo.x){
-	    enemyTwo.x = 700;
-	}
-	if(barfX > enemyThree.x){
-	    enemyThree.x = 800;
-	}
+    }
 
-	if (barfX > 800) {
-	    barfFired = false;
-	    barfX = 0;
-	}
-    };
+    if((barfX > enemyOne.x)
+       && (barfY > enemyOne.y)
+       && (barfY < (enemyOne.y + 30))){
+	enemyOne.x = 625;
+    }
+
+    if(barfX > enemyTwo.x){
+	enemyTwo.x = 700;
+    }
+
+    if(barfX > enemyThree.x){
+	enemyThree.x = 800;
+    }
+
+    if (barfX > 800) {
+	barfFired = false;
+	barfX = 0;
+    }
+
+    if (barfY > enemyTwo.y){
+	enemyTwo.y = 225;
+    }
+
+    if (barfY > enemyThree.y){
+	enemyThree.y = 225;
+    }
+};
 
